85b9a51073268fb7b6e6fb81a891e7b626a89013
Guilherme Porto <guiportooo@gmail.com>
2021-04-25 18:22:32 -0300

    Add Header and assets

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import 'react-native-gesture-handler'
 import React from 'react'
+import { Platform, StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { useFonts } from 'expo-font'
+import AppLoading from 'expo-app-loading'
+import Header from './Header'
 import Homepage from './Home'
 
 const Stack = createStackNavigator()
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Globomantics'>
-        <Stack.Screen name='Globomantics' component={Homepage} />
+    <NavigationContainer
+      style={{
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <Stack.Navigator initialRouteName='Globomantics' headerMode='screen'>
+        <Stack.Screen
+          name='Globomantics'
+          component={Homepage}
+          options={{ header: () => <Header headerDisplay='Globomantics' /> }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import logo from './assets/globomantics-logo-bug-black.png'
+
+export default function Header(props) {
+  return (
+    <View style={styles.header}>
+      <Image source={logo} style={{ width: 35, height: 35 }} />
+      <View>
+        <Text style={styles.text}>{props.headerDisplay}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 70,
+    marginTop: getStatusBarHeight(),
+  },
+  text: {
+    fontFamily: 'OpenSans',
+  },
+})
